b2be1f05ce25df2c98aa97f39faffe899344dfa3
cgranda <Cesar.Granda@baruch.cuny.edu>
2023-07-20 16:36:49 -0400

    added the delete button to covers

--- src/pages/CoverDetails.js
+++ src/pages/CoverDetails.js
@@ -18,11 +18,16 @@ const CoverDetails = () => {
     const { coverId } = useParams();
 
     const { data, isFetching } = useGetCoverQuery(coverId);
-    // console.log(data);
+    const coverIndex = data?.data?._id;
+    console.log(coverIndex);
 
-
-    const [message, setMessage] = useState(null);
+    const [messageG, setMessageG] = useState(null);
     const [messagePdf, setMessagePdf] = useState(null);
+    const [messageDel, setMessageDel] = useState(null);
+    // const [bio, setbio] = useState([]);
+    // useEffect(()=> {
+    //   bioMDB();
+    // }, []);
 
     const handleClick = async () => {
         try {
@@ -30,7 +35,7 @@ const CoverDetails = () => {
             `${process.env.REACT_APP_API_URL}/pdf/covers/${coverId}`,
           );
           const result = await fetch(url, { method: 'HEAD' });
-            setMessage(response.status);
+            setMessageG(response.status);
             console.log(response.status);
         } catch (err) {
             // catch any unexpected errors
@@ -42,45 +47,64 @@ const CoverDetails = () => {
     const pdfUrl = `https://cover-letter-mern-back.onrender.com/uploads/cover${coverId}.pdf`;
     const pdfName = `${coverId}.pdf`;
 
+    // const bioMDB = async () => {
+    //     const response = await fetch(pdfUrl, { method: 'HEAD' });
+
+    //     setbio(await response.json());
+    // };
+
     const downloadFile = async () => {
       const result = await fetch(pdfUrl, { method: 'HEAD' });
       setMessagePdf(result.status);
       console.log(result.status);
     };
-    // const delFile = async () => {
 
-    //   try {
-    //     const l = `${process.env.REACT_APP_API_URL}/covers/${coverId}`;
-    //     console.log(l);
-    //     const result = await fetch(l, {
-    //      method: 'DELETE',
-    //     });
-    //       // setMessagePdf(result.status);
-    //       console.log(result.status);
-    //   } catch (err) {
-    //         // catch any unexpected errors
-    //         // setMessage(response.status);
-    //         console.log(err);
-    //   }
-    // };
+    const l = `${process.env.REACT_APP_API_URL}/del/covers/${coverIndex}`;
+    const delFile = async () => {
+        try {
+            const fetchResult = await fetch(l, {
+             method: 'DELETE',
+            });
+            const result = await fetchResult.json();
+
+            if (fetchResult.ok) {
+              setMessageDel(result.message);
+              return result;
+            }
+            const responseError = {
+              type: 'Error',
+              message: result.message || 'Something went wrong',
+              data: result.data || '',
+              code: result.code || '',
+            };
+
+            let error = new Error();
+            error = { ...error, ...responseError };
+            throw (error);
+        } catch (e) {
+            setMessageDel('Failed deleting cover', e);
+            console.log('Failed deleting cover', e); // handle error
+        }
+    };
+
+    // console.log(bio.message);
     return (
         <>
         <ScrollView>
             <Header />
             <Container>
 
-                <button className="click" onClick={handleClick}>
-                  Generate Pdf
-                </button>
+                <button className="click" onClick={handleClick}>Generate Pdf</button>
                 <button className="click" onClick={downloadFile}>
                   view Pdf
                 </button>
                 {messagePdf == 404 && <div>No Pdf found</div>}
                 {messagePdf == 200 && <div><a href={pdfUrl} download={pdfName}>Download Cover Letter Pdf</a></div>}
                 <CoverOnly/>
-                {/* <button className="click" onClick={delFile}>
+                <button className="click del" onClick={delFile}>
                   Delete Cover Letter
-                </button>*/}
+                </button>
+                <div>{messageDel}</div>
             </Container>
         </ScrollView>
         <Footer />
